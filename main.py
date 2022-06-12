9f0d9d20291cf81611971a96aee9d31de7757333
Berke <69055194+fsb3rke@users.noreply.github.com>
2022-06-12 22:15:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ TOKEN = "" # ACCESS KEY
 
 today = datetime.datetime.today()
 now_time = datetime.datetime.now()
+while True:
+    if now_time.hour == 00 or now_time.hour == 0:
+        a = requests.get(f"https://api.unsplash.com/photos/random?count=1&client_id={TOKEN}")
+        m_a = json.loads(str(json.dumps(a.json()[0])))
 
-while now_time.hour == 00:
-    a = requests.get(f"https://api.unsplash.com/photos/random?count=1&client_id={TOKEN}")
-    m_a = json.loads(str(json.dumps(a.json()[0])))
+        photo_description = m_a["description"]
+        photo_url = m_a["urls"]["raw"]
 
-    photo_description = m_a["description"]
-    photo_url = m_a["urls"]["raw"]
+        img_blob = requests.get(photo_url).content
+        strToday = today.strftime("%Y-%m-%d")
+        with open(f"images/{strToday}.jpg", "wb") as img_file:
+            img_file.write(img_blob)
 
-    img_blob = requests.get(photo_url).content
-    strToday = today.strftime("%Y-%m-%d")
-    with open(f"images/{strToday}.jpg", "wb") as img_file:
-        img_file.write(img_blob)
-
-    ctypes.windll.user32.SystemParametersInfoA(20, 0, f"{os.getcwd()}\\images\\{strToday}.jpg", 0)
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, f"{os.getcwd()}\\images\\{strToday}.jpg", 0)
