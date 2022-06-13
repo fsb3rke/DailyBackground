b0123e263db0e22755888486862f618db233fe1a
Berke <69055194+fsb3rke@users.noreply.github.com>
2022-06-13 16:55:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,12 +4,30 @@ import datetime
 import ctypes
 import os
 
+def change_last_date(new_date: datetime):
+    new_date_formatted = new_date.strftime('%Y-%m-%d')
+    last_date_content = open("lastdate.json", "r").read()
+    last_date_json = json.loads(last_date_content)
+    last_date_json["date"] = new_date_formatted
+    last_date_dumps = json.dumps(last_date_json)
+    open("lastdate.json", "w").write(last_date_dumps)
+
+def check_is_date_different(current_date: datetime):
+    current_date_formatted = current_date.strftime('%Y-%m-%d')
+    last_date_content = open("lastdate.json", "r").read()
+    last_date_json = json.loads(last_date_content)
+    last_date_formatted = last_date_json["date"]
+    if current_date_formatted != last_date_formatted:
+        return True
+    else:
+        return False
+
+
 TOKEN = "" # ACCESS KEY
 
 today = datetime.datetime.today()
-now_time = datetime.datetime.now()
 while True:
-    if now_time.hour == 00 or now_time.hour == 0:
+    if check_is_date_different(today):
         a = requests.get(f"https://api.unsplash.com/photos/random?count=1&client_id={TOKEN}")
         m_a = json.loads(str(json.dumps(a.json()[0])))
 
@@ -22,3 +40,4 @@ while True:
             img_file.write(img_blob)
 
         ctypes.windll.user32.SystemParametersInfoW(20, 0, f"{os.getcwd()}\\images\\{strToday}.jpg", 0)
+        change_last_date(today)
