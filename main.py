0fa91e5f49c449b137b109eadefac2d0997f8c6f
Berke <69055194+fsb3rke@users.noreply.github.com>
2022-06-14 17:39:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,28 @@ import datetime
 import ctypes
 import os
 import time
+from win10toast import ToastNotifier
+
+def time_format(seconds: int):
+    if seconds is not None:
+        seconds = int(seconds)
+        d = seconds // (3600 * 24)
+        h = seconds // 3600 % 24
+        m = seconds % 3600 // 60
+        s = seconds % 3600 % 60
+        if d > 0:
+            return '{:02d}D {:02d}H {:02d}m {:02d}s'.format(d, h, m, s)
+        elif h > 0:
+            return '{:02d}H {:02d}m {:02d}s'.format(h, m, s)
+        elif m > 0:
+            return '{:02d}m {:02d}s'.format(m, s)
+        elif s > 0:
+            return '{:02d}s'.format(s)
+    return '-'
+
+def send_notification(notification_title: str, notification_body: str, duration: int, icon_path: str, threaded: bool):
+    toast = ToastNotifier()
+    toast.show_toast(notification_title, notification_body, duration=duration, icon_path=icon_path, threaded=threaded)
 
 def calculate_remaining_time(date: datetime):
     date_formatted = date.strftime("%H-%M-%S")
@@ -58,4 +80,7 @@ while True:
         ctypes.windll.user32.SystemParametersInfoW(20, 0, f"{os.getcwd()}\\images\\{strToday}.jpg", 0)
         change_last_date(today)
         today = datetime.datetime.today()
+
+        send_notification("Daily Background", f"Background has been updated!\n\nYour background is will be change after {time_format(calculate_remaining_time(today))}", 15, "bin/icon.ico", True)
+
     time.sleep(calculate_remaining_time(today))
